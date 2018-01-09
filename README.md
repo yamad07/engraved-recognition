22777749793ea5b0b7af4f12e8999aa8f16798fd
yamad07 <yuhsukeshootsfc@gmail.com>
2018-01-10 01:13:00 +0900

    [add] default_boxをモデルの構造から算出するコードを追加

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
 # engraved-recognition
 
 ## Usage
-1.anaconda環境を構築
+1.anaconda環境を構築する
 [参考](https://qiita.com/sk427/items/9f215931c8249ada75cd)
 
-2.下のライブラリをインストール。
+2.仮想環境を作る
 
-3.サーバーを起動
+```
+$ conda env create --file env.yml
+```
+
+3.仮想環境に入る
+
+```
+$ source activate recarat-flask-api
+```
+4.サーバーを起動する
 
 ```
 $ cd server
 $ FLASK_APP=main.py flask run
 ```
 
-4.別ウィンドウでサンプルの写真を使ってcurlコマンドで叩く
+5.別ウィンドウでサンプルの写真を使ってcurlコマンドで叩く
 ```
 curl -X POST http://localhost:5000/recognition -F "img=@./sample.jpg"
 ```
-
-## Library Version
-anaconda3-4.4.0
-flask 0.12.2
-flask_restful
-pytorch 0.3.0.post4
-torchvision 0.2.0
-opencv 3.1.0
--- modules/encoder.py
+++ modules/encoder.py
@@ -1,6 +1,36 @@
 import torch
+import math
+import itertools
 
 class DataEncoder:
+    def __init__(self):
+        scale = 300.
+        steps = [s / scale for s in (8, 16, 32, 64, 100, 300)]
+        sizes = [s / scale for s in (30, 60, 111, 162, 213, 264, 315)]
+        aspect_ratios = ((2,), (2,3), (2,3), (2,3), (2,), (2,))
+        feature_map_sizes = (37, 18, 9, 5, 3, 1)
+
+        num_layers = len(feature_map_sizes)
+
+        boxes = []
+        for i in range(num_layers):
+            fmsize = feature_map_sizes[i]
+            for h,w in itertools.product(range(fmsize), repeat=2):
+                cx = (w + 0.5) * steps[i]
+                cy = (h + 0.5) * steps[i]
+
+                s = sizes[i]
+                boxes.append((cx, cy, s, s))
+
+                s = math.sqrt(sizes[i] * sizes[i+1])
+                boxes.append((cx, cy, s, s))
+
+                s = sizes[i]
+                for ar in aspect_ratios[i]:
+                    boxes.append((cx, cy, s * math.sqrt(ar), s / math.sqrt(ar)))
+                    boxes.append((cx, cy, s / math.sqrt(ar), s * math.sqrt(ar)))
+
+        self.default_boxes = torch.Tensor(boxes)
 
     def nms(self, bboxes, scores, threshold=0.5, mode='union'):
         x1 = bboxes[:,0]
