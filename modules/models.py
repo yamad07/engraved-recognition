1a07e141bf93b55bee1ecb31484bbdfea982d95f
yamad07 <yuhsukeshootsfc@gmail.com>
2018-01-10 00:36:31 +0900

    [remove] debug用のプリント文を削除した

--- modules/models.py
+++ modules/models.py
@@ -16,8 +16,6 @@ class RecognitionModel:
 
     def run(self, img):
         img_resized = img.resize((300, 300))
-        print(img)
-        print(img_resized.size)
         transform = transforms.Compose([
                                         transforms.Resize((300, 300)),
                                         transforms.ToTensor(),
@@ -27,14 +25,10 @@ class RecognitionModel:
         data_encoder = DataEncoder()
         boxes, _, _ = data_encoder.decode(loc.data.squeeze(0), F.softmax(conf.squeeze(0)).data)
         boxes_list = []
-        print(boxes)
         for box in boxes:
-            print(img.width)
-            print(img.height)
             box[::2] *= img.width
             box[1::2] *= img.height
             box_np= box.numpy()
             box_np = box_np.astype(np.int64)
-            print(box_np)
             boxes_list.append(box_np.tolist())
         return boxes_list
